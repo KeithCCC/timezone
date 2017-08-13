e92c72ebb4ffa8ef7445ff5c1da0577c509c4576
KC <keithc2chen@gmail.com>
2017-08-14 00:53:37 +0900

    app.py with js

--- app.py
+++ app.py
@@ -10,13 +10,23 @@ def cattime():
     tz_tokyo = pytz.timezone('Asia/Tokyo')
     tz_sp = pytz.timezone('US/Eastern')
     tz_paris = pytz.timezone('Europe/Paris')
-    cat1now=datetime.now(tz_tokyo).strftime("%m/%d %H:%M:%S")
-    cat2now=datetime.now(tz_sp).strftime("%m/%d %H:%M:%S")
-    cat3now=datetime.now(tz_paris).strftime("%m/%d %H:%M:%S")
-    timenow=datetime.now().strftime("%m/%d %H:%M:%S")
-    return render_template('cattime2.html',cat1=cat1now,cat2=cat2now,cat3=cat3now,timenow=timenow)
+    cat1now=datetime.now(tz_tokyo).strftime("%m/%d %H:%M")
+    cat2now=datetime.now(tz_sp).strftime("%m/%d %H:%M")
+    cat3now=datetime.now(tz_paris).strftime("%m/%d %H:%M")
+    timenow=datetime.now().strftime("%m/%d %H:%M")
+    return render_template('cattime3.html',cat1=cat1now,cat2=cat2now,cat3=cat3now,timenow=timenow)
 
 @app.route("/<timezone>")
 def hello(timezone=None):
     TZtime = datetime.utcnow()
     return render_template('timezone.html',timezone=timezone, TZtime=TZtime)
+
+
+@app.route("/test/")
+def template_test():
+    return render_template('timejs.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
