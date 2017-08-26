9850664d53f001623d7322a0062d2b8b512acd9a
KC <keithc2chen@gmail.com>
2017-08-26 23:46:05 +0900

    app.py

--- app.py
+++ app.py
@@ -1,32 +1,61 @@
-from flask import Flask
-from flask import render_template
-from datetime import datetime, tzinfo
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime, tzinfo, timedelta
 import pytz
 
 app = Flask(__name__)
+Bootstrap(app)
+ 
+def BuildTimezoneList(time1st):
+    #time.strftime("%m/%d %H:%M")
+    timelist = [time1st.strftime("%m/%d (%a) %H:%M")]
+    for i in range(24):
+        time1st = time1st + timedelta(hours=1)
+        timelist.append(time1st.strftime("%m/%d (%a) %H:%M"))
+    return timelist
 
 @app.route("/")
-def cattime():
+def index():
+    return render_template('test.html')
+
+@app.route("/div")
+def CatTimeDiv():
     tz_tokyo = pytz.timezone('Asia/Tokyo')
     tz_sp = pytz.timezone('US/Eastern')
     tz_paris = pytz.timezone('Europe/Paris')
-    cat1now=datetime.now(tz_tokyo).strftime("%m/%d %H:%M")
-    cat2now=datetime.now(tz_sp).strftime("%m/%d %H:%M")
-    cat3now=datetime.now(tz_paris).strftime("%m/%d %H:%M")
+    cat1list=BuildTimezoneList(datetime.now(tz_tokyo))
+
+    cat2list=BuildTimezoneList(datetime.now(tz_sp))
+    cat3list=BuildTimezoneList(datetime.now(tz_paris))
     timenow=datetime.now().strftime("%m/%d %H:%M")
-    return render_template('cattime3.html',cat1=cat1now,cat2=cat2now,cat3=cat3now,timenow=timenow)
+    return render_template('TZ_div.html',cat1=cat1list,cat2=cat2list,cat3=cat3list,timenow=timenow)
 
-@app.route("/<timezone>")
-def hello(timezone=None):
-    TZtime = datetime.utcnow()
-    return render_template('timezone.html',timezone=timezone, TZtime=TZtime)
+@app.route("/t")
+def CatTimeTbl():
+    tz_tokyo = pytz.timezone('Asia/Tokyo')
+    tz_sp = pytz.timezone('US/Eastern')
+    tz_paris = pytz.timezone('Europe/Paris')
+    cat1list=BuildTimezoneList(datetime.now(tz_tokyo))
+
+    cat2list=BuildTimezoneList(datetime.now(tz_sp))
+    cat3list=BuildTimezoneList(datetime.now(tz_paris))
+    timenow=datetime.now().strftime("%m/%d %H:%M")
+    return render_template('TZ_table.html',cat1=cat1list,cat2=cat2list,cat3=cat3list,timenow=timenow)
+
+@app.route("/t1")
+def CatTimeTbl1():
+    tz_tokyo = pytz.timezone('Asia/Tokyo')
+    tz_sp = pytz.timezone('US/Eastern')
+    tz_paris = pytz.timezone('Europe/Paris')
+    cat1list=BuildTimezoneList(datetime.now(tz_tokyo))
+
+    cat2list=BuildTimezoneList(datetime.now(tz_sp))
+    cat3list=BuildTimezoneList(datetime.now(tz_paris))
+    timenow=datetime.now().strftime("%m/%d %H:%M")
+    return render_template('TZ_t1.html',cat1=cat1list,cat2=cat2list,cat3=cat3list,timenow=timenow)
 
 
-@app.route("/test/")
-def template_test():
-    return render_template('timejs.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
